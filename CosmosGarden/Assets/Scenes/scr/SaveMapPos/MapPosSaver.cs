87a63647a048fe8f1ef06f8db172f16757d6331b
dpa32 <128779850+dpa32@users.noreply.github.com>
2024-07-11 10:58:37 +0900

    .

---
+++ CosmosGarden/Assets/Scenes/scr/SaveMapPos/MapPosSaver.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MapPosSaver : MonoBehaviour
+{
+    private EditTile _EditTile;
+    public GameData_t _MapSaveData;
+
+    public List<Vector3Int> LandPosToSave;
+    public List<Vector3Int> ObjPosToSave;
+
+    public void SaveMapData()
+    {
+        _EditTile = GameObject.Find("SetObjManager").GetComponent<EditTile>();
+        LandPosToSave = _EditTile.LandPos;
+        ObjPosToSave = _EditTile.ObjPos;
+
+        _MapSaveData = new GameData_t();
+
+        _MapSaveData.LandPosX = new int[LandPosToSave.Count];
+        _MapSaveData.LandPosY = new int[LandPosToSave.Count];
+        _MapSaveData.LandPosZ = new int[LandPosToSave.Count];
+
+        _MapSaveData.ObjPosX = new int[ObjPosToSave.Count];
+        _MapSaveData.ObjPosY = new int[ObjPosToSave.Count];
+        _MapSaveData.ObjPosZ = new int[ObjPosToSave.Count];
+
+        int i = 0;
+        foreach (Vector3Int pos in LandPosToSave)
+        {
+            _MapSaveData.LandPosX[i] = pos.x;
+            _MapSaveData.LandPosY[i] = pos.y;
+            _MapSaveData.LandPosZ[i] = pos.z;
+            i++;
+        }
+
+        i = 0;
+        foreach(Vector3Int pos in ObjPosToSave)
+        {
+            _MapSaveData.ObjPosX[i] = pos.x;
+            _MapSaveData.ObjPosY[i] = pos.y;
+            _MapSaveData.ObjPosZ[i] = pos.z;
+            i++;
+        }
+
+    }
+}
+
+public class GameData_t
+{
+    public int[] LandPosX;
+    public int[] LandPosY;
+    public int[] LandPosZ;
+
+    public int[] ObjPosX;
+    public int[] ObjPosY;
+    public int[] ObjPosZ;
+}
